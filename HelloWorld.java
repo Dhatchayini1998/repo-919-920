58e5832556d0300ca4e7844250fc244fc2e3691f
Dhatchayini1998 <65335542+Dhatchayini1998@users.noreply.github.com>
2021-03-24 18:01:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld{
 public static void main(String []args){
    System.out.println("Hello World");
   }
-   public Static void add(int num1 , in num2){
+   public static void add(int num1 , in num2){
      System.out.println(num1+num2);
    }
 }
